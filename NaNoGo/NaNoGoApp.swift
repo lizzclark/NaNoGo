7c042aa62457e7a1150f08b54f19aac25550cc49
Lizz Clark <lizz.clark@moonpig.com>
2020-07-14 16:14:41 +0100

    Initial Commit

---
+++ NaNoGo/NaNoGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaNoGoApp.swift
+//  NaNoGo
+//
+//  Created by Lizz Clark on 14/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct NaNoGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
